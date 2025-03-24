d740a8a9e2a01529b3dbc22e8fc552fc2f90f747
JoustenBlanco <jousten.blanco.fonseca@est.una.ac.cr>
2025-03-24 16:13:14 -0600

    [ADD] Students reports api

---
+++ src/app/api/reportes/estudiantes/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../lib/prisma";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const nombre = searchParams.get("nombre") || "";
+    const cedula = searchParams.get("cedula") || "";
+
+    const reportData = await prisma.rAE_Alumnos.findMany({
+      where: {
+        Cedula: {
+          contains: cedula,
+        },
+        Primer_nombre: {
+          contains: nombre,
+        },
+      },
+      include: {
+        RAE_Asistencia_X_Alumnos: true,
+      },
+    });
+
+    return NextResponse.json(reportData, { status: 200 });
+  } catch (error) {
+    console.error("Error al obtener reporte:", error);
+    return NextResponse.json(
+      { error: "Error al obtener los datos del reporte" },
+      { status: 500 }
+    );
+  }
+}
