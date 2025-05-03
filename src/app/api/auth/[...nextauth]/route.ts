371e154aef89734db49228606313977a5f945a42
JoustenBlanco <jousten.blanco.fonseca@est.una.ac.cr>
2025-05-03 10:43:08 -0600

    [FIX] Remove handler on api auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth-options";
 
-const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
